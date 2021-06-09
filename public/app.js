0ba44be149573c9080d1c6a4280e2b6fdd607dfa
misztijanos <misztijanos@yahoo.com>
2021-06-09 11:35:08 +0300

    finished

--- public/app.js
+++ public/app.js
@@ -1,1 +1,22 @@
-"use strict";
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+//list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
+});
